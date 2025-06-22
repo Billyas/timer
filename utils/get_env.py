d7b7168ee7e977596f63fae1958b7121765212cc
Billyas <62041096+Billyas@users.noreply.github.com>
2025-06-22 21:57:39 +0800

    Update get_env.py

--- utils/get_env.py
+++ utils/get_env.py
@@ -8,10 +8,9 @@ import re
 
 from dotenv import load_dotenv, find_dotenv
 
+from .fn_print import fn_print
 
-from typing import *
 
-all_print_list = []
 
 def get_env(env_var, separator):
     if env_var in os.environ:
@@ -23,16 +22,4 @@ def get_env(env_var, separator):
         else:
             fn_print(f"未找到{env_var}变量.")
             return []
-def fn_print(*args, sep=' ', end='\n', **kwargs):
-    global all_print_list
-    output = ""
-    # 构建输出字符串
-    for index, arg in enumerate(args):
-        if index == len(args) - 1:
-            output += str(arg)
-            continue
-        output += str(arg) + sep
-    output = output + end
-    all_print_list.append(output)
-    # 调用内置的 print 函数打印字符串
-    print(*args, sep=sep, end=end, **kwargs)
+
